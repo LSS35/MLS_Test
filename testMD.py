27101201bd5fb323f57400f09201d5989ba216f5
LSS35 <47209405+LSS35@users.noreply.github.com>
2019-01-31 16:47:52 +0300

    Add files via upload

---
+++ testMD.py
@@ -0,0 +1,24 @@
+﻿from selenium import webdriver
+import datetime
+import io
+
+driver = webdriver.Chrome()
+driver.get("https://yandex.ru/time/")
+
+ya = driver.title
+yas = ya[0:5]
+
+now = datetime.datetime.now()
+loc = now.strftime("%H:%M")
+
+
+str = u"≠"
+file =  io.open("log.txt", "w+", encoding="utf-8") 
+
+if loc == yas:
+    file.write(now.strftime("%H:%M:%S")+" - УСПЕХ: "+loc+" = "+yas)
+else:
+    file.write(now.strftime("%H:%M:%S")+" - ОШИБКА: "+loc+" "+str+" "+yas)
+
+file.close()        
+driver.close()
